0aa35a4669f8a18301434c3ceb517c76e57c9a26
OSCARMENDOZA97 <mw529102@gmail.com>
2025-06-05 12:40:40 -0500

    Parameters_I

--- PYREVIT/OM-BIM.extension/OMBIM.tab/LearnRevitApi.panel/col1.stack/Module 03.pulldown/03.02. Read Properties.pushbutton/a.py
+++
@@ -1,3 +0,0 @@
-resultado2 = [num + 2 for num in range(10)]
-
-print(resultado2)
--- PYREVIT/OM-BIM.extension/OMBIM.tab/PRUEBA.panel/FirstButton.pushbutton/script.py
+++
@@ -1,5 +0,0 @@
-from example import test_print
-from example import test_print
-
-# Get select elements
-test_print()
